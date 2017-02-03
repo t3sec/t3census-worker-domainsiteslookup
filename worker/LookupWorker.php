a9a836072fedf116959c0f791e1f62ab19fcf5e1
Marcus Krause <marcus@t3sec.info>
2017-02-03 21:44:32 +0100

    [TASK] Code cleanup (whitespace changes only)

--- worker/LookupWorker.php
+++ worker/LookupWorker.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Gelf\Publisher;
 use Gelf\Transport\UdpTransport;
@@ -9,7 +9,6 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Handler\GelfHandler;
 use T3sec\BingScraper\ScraperSearch;
 use T3sec\BingScraper\Exception\EmptyBodyException;
-
 
 $logfile = __DIR__ . '/../t3census-worker-domainsiteslookup.log';
 
@@ -25,53 +24,54 @@ $worker->addFunction('DomainSitesLookup', 'fetchSites');
 $worker->setTimeout(5000);
 
 while (1) {
-	try {
-		$worker->work();
-	} catch (Exception $e) {
-		fwrite(STDERR, sprintf('ERROR: Job-Worker: %s (Errno: %u)' . PHP_EOL, $e->getMessage(), $e->getCode()));
-		$logger->addError($e->getMessage(), array('errorcode' => $e->getCode()));
-		exit(1);
-	}
+    try {
+        $worker->work();
+    } catch (Exception $e) {
+        fwrite(STDERR, sprintf('ERROR: Job-Worker: %s (Errno: %u)' . PHP_EOL, $e->getMessage(), $e->getCode()));
+        $logger->addError($e->getMessage(), array('errorcode' => $e->getCode()));
+        exit(1);
+    }
 
-	if ($worker->returnCode() == GEARMAN_TIMEOUT) {
-		//do some other work here
-		continue;
-	}
-	if ($worker->returnCode() != GEARMAN_SUCCESS) {
-		// do some error handling here
-		exit(1);
-	}
+    if ($worker->returnCode() == GEARMAN_TIMEOUT) {
+        //do some other work here
+        continue;
+    }
+    if ($worker->returnCode() != GEARMAN_SUCCESS) {
+        // do some error handling here
+        exit(1);
+    }
 }
 
 
-function fetchSites(GearmanJob $job) {
-	global $logger;
+function fetchSites(GearmanJob $job)
+{
+    global $logger;
 
-	$domain = $job->workload();
-	$logger->addDebug('Processing domain', array('domain' => $domain));
+    $domain = $job->workload();
+    $logger->addDebug('Processing domain', array('domain' => $domain));
 
-	try {
-		$objLookup = new ScraperSearch();
-		$objLookup->setEndpoint('http://www.bing.com/search')->setMaxResults(1000);
-		$results = $objLookup->setQuery('site:' . $domain)->getResults();
-		unset($objLookup);
-	} catch (EmptyBodyException $e) {
-		$logger->addWarning($e->getMessage(), array('errorcode' => $e->getCode(), 'domain' => $domain));
-		$job->sendData(Logger::WARNING . ' ' . $e->getMessage());
-		$job->sendFail();
-		return;
-	} catch (\Exception $e) {
-		$logger->addError($e->getMessage(), array('errorcode' => $e->getCode(), 'domain' => $domain));
-		$job->sendData(Logger::ERROR . ' ' . $e->getMessage());
-		$job->sendException($e->getMessage());
-		return;
-	}
+    try {
+        $objLookup = new ScraperSearch();
+        $objLookup->setEndpoint('http://www.bing.com/search')->setMaxResults(1000);
+        $results = $objLookup->setQuery('site:' . $domain)->getResults();
+        unset($objLookup);
+    } catch (EmptyBodyException $e) {
+        $logger->addWarning($e->getMessage(), array('errorcode' => $e->getCode(), 'domain' => $domain));
+        $job->sendData(Logger::WARNING . ' ' . $e->getMessage());
+        $job->sendFail();
+        return;
+    } catch (\Exception $e) {
+        $logger->addError($e->getMessage(), array('errorcode' => $e->getCode(), 'domain' => $domain));
+        $job->sendData(Logger::ERROR . ' ' . $e->getMessage());
+        $job->sendException($e->getMessage());
+        return;
+    }
 
-	if (!empty($results)) {
-		$logger->addInfo('Retrieved sites for domain', array('domain' => $domain, 'urls' => $results, 'count' => count($results)));
-	}
+    if (!empty($results)) {
+        $logger->addInfo('Retrieved sites for domain', array('domain' => $domain, 'urls' => $results, 'count' => count($results)));
+    }
 
-	return json_encode($results);
+    return json_encode($results);
 }
 
 ?>
